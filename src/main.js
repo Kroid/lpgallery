f27563fda15714b1d0d274afc4a446922a9abd97
George Mironov <kroid@yandex.ru>
2020-05-01 19:27:54 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -12,23 +12,4 @@ export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.use(VueThinModal)
   Vue.component('Layout', DefaultLayout)
-
-  router.afterEach((to, from) => {
-    if (isClient) {
-      var interval = setInterval(function() {
-        if (typeof window.dojoRequire !== 'undefined' && !window.mailchimpInitialized) {
-          window.dojoRequire(['mojo/signup-forms/Loader'], function(L) {
-            L.start({
-              baseUrl: 'mc.us8.list-manage.com',
-              uuid: '9c9d79402355b38121d6060c9',
-              lid: '7968226263',
-              uniqueMethods: true,
-            })
-          })
-          window.mailchimpInitialized = true
-          clearInterval(interval)
-        }
-      }, 50)
-    }
-  })
 }
